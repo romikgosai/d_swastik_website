6185b5e4e31edc74ddb093be92427f5d7680f8de
romik-ank <romik@ank-world.com>
2026-02-01 16:17:24 +0545

    Fix asset paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
   images: { unoptimized: true },
   output: 'export',
   distDir: 'dist',
+  basePath: process.env.NODE_ENV === 'production' ? '/d_swastik_website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/d_swastik_website' : '',
 };
 
 module.exports = nextConfig;
